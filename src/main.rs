86fc84eeec0cfd85ce70aef5aa105619ec816f6f
saylor <sailsman@tutanota.com>
2024-07-01 00:00:14 +0200

    POC reading player x from memory

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use process_memory::ProcessHandle;
-use sysinfo::{System, Process};
+use sysinfo::{System};
 use process_memory::{copy_address, TryIntoProcessHandle};
 use std::fs::File;
 use std::io::Read;
-use std::i64;
 use std::usize;
 use std::process::exit;
 use patternscan::scan;
@@ -29,49 +28,78 @@ fn main() {
     f.read_to_string(&mut buffer).expect("should work");
     // println!("{}", buffer);
     let mut keep_going = false;
+
+    let mut valid_proc = Vec::new();
     for address_space in buffer.split("\n") {
         // println!("{}", address_space);
         if !address_space.contains("DarkSoulsRemastered.exe") && !(keep_going && (!address_space.contains("/") && !address_space.contains("["))) {
             keep_going = false;
             continue;
         }
+        valid_proc.push(address_space);
         keep_going = true;
-        // println!("pass for {}", address_space);
+    }
+    // println!("pass for {}", address_space);
+    for address_space in valid_proc {
         let mut memory_range = address_space.split(" ").next().expect("First should always be there").split("-");
         let start_address = match usize::from_str_radix(memory_range.next().expect("should also be there"), 16) {
             Ok(v) => v,
-            Err(e) => {println!("error getting start_address, {}", e);continue;}
+            Err(e) => {
+                println!("error getting start_address, {}", e);
+                continue;
+            }
         };
         let end_address = match usize::from_str_radix(memory_range.next().expect("shuld be here if the first one is here"), 16) {
             Ok(v) => v,
-            Err(e) =>  {/*println!("error getting end_address, {}", e);*/continue;}
+            Err(e) => { /*println!("error getting end_address, {}", e);*/continue; }
         };
         let size = end_address - start_address;
         // println!("{} with size {}",  start_address, size);
 
         let _bytes = match copy_address(start_address, size, &handle) {
             Ok(v) => v,
-            Err(e) => {/*println!("error actually reading memory: {}", e);*/continue;}
+            Err(e) => { /*println!("error actually reading memory: {}", e);*/continue; }
         };
         // println!("read {} bytes", _bytes.len());
 
         let pattern = "48 8b 0d ? ? ? ? 0f 28 f1 48 85 c9 74 ? 48 89 7c";
         let locs = match scan(Cursor::new(_bytes), &pattern) {
             Ok(v) => v,
-            Err(e) => {println!("error getting locations: {}", e);continue}
+            Err(e) => {
+                println!("error getting locations: {}", e);
+                continue
+            }
         };
 
-        if (locs.len() > 0) {
+        if locs.len() > 0 {
             println!("found occurence in {} at offset {:?}", address_space, locs[0]);
         } else {
             continue;
         }
 
-        let offset = vec![0, 0x68, 0x68, 0x28];
+        let ad = locs[0] + 5368713216 + 3;
+        let _bytes = match copy_address(ad, 4, &handle) {
+            Ok(v) => v,
+            Err(e) => { exit(69); }
+        };
+        let addr_at_ini = i32::from_le_bytes(_bytes.try_into().expect(""));
+        let target = 5368713216 + locs[0] + addr_at_ini as usize + 7;
 
-        let final_address = resolve_offsets_to_final_address(locs[0], offset, handle);
+        let offset = vec![0, 0x68, 0x68, 0x28, 0x10];
+
+        let final_address = resolve_offsets_to_final_address(target, offset, handle);
         println!("final address: {}", final_address);
-        // println!("trying to read the offset");
+
+        let abc = match copy_address( final_address, 4, &handle) {
+            Ok(v) => v,
+            Err(e) => {exit(69);}
+        };
+        let x = f32::from_le_bytes(abc.try_into().expect(""));//process.read_mem::<usize>(address).unwrap();
+
+
+
+
+         println!("trying to read the offset");
 
         // filepath = format!("/proc/{}/maps", memory_range);
         // let mut buffer = Vec::new();
